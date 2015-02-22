ed4b95a82b54d188b7af9167df0aec15d99f3f61
megastef <s.thies@bigdata-analyst.de>
2015-02-22 23:50:15 +0100

    complete rewrite using hoxy

--- index.js
+++ index.js
@@ -1,76 +1,36 @@
-var http = require('http'),
-    connect = require('connect'),
-    request = require('request'),
-    colors = require('colors'),
-    util = require('util'),
-    httpProxy = require('http-proxy'),
-    proxy = httpProxy.createProxyServer({})
+var colors = require ('colors')
+    hoxy = require('hoxy'),
     util = require ('util')
-    useColors = false
-    argv = require('minimist')(process.argv.slice(2));
+    argv = require('minimist')(process.argv.slice(2))
+    target = argv['h'] || 'htpp://localhost:9200'
+    useColors = argv['c'] || false
+    port = argv['p'] || 9100
+    proxy = new hoxy.Proxy({reverse: target}).listen(port, function(){
+       console.log('proxy listen ' + port);
+    })
 
-var target = argv['h'] || 'htpp://localhost:9200'
-var useColors = argv['c'] 
-var port = argv['p'] || 9100
-
-//
-//  Basic Http Proxy Server
-//
-var app = connect()
-  .use(function(req, res){
-    req.ts = new Date()
-    var _write = res.write;
-    res.body = ''
-    res.write = function (data) {
-      if (data)
-        res.body += data.toString()
-      _write.call(res, data);
-    }
-
-    var _read = req.read 
-    req.body = ''
-    req.read = function () {
-      
-      var data = _read.call(req);
-      if (data)
-        req.body += data.toString()
-      return data
-
-    }
-    var _end = res.end
-    res.end = function (data)
-    {
-        _end.call(res, data)
-        var color =  'red'
+proxy.intercept({
+  phase: 'response',
+  //fullUrl: 'http://example.com/api/users/:id',
+  //mimeType: 'application/json',
+  as: 'string'
+}, function(req, res){
+   var color =  'red'
         if (res.statusCode < 400)
             color = 'green'
-        if (useColors == true) {
-          var reqMsg = util.format ('%s REQ %s\n\t%s', req.ts.toISOString(), req.method + ' ' + req.url, req.body.yellow ).yellow
-          var resMsg = util.format ('%s RES %s\n\t%s'[color], new Date().toISOString(), res.statusCode, res.body[color] )
+        if (useColors == 'true') 
+        {
+          var reqMsg = util.format ('%s REQ %s\n\t%s', new Date().toISOString(), req.method + ' ' + req.url, req.string.yellow ).yellow
+          var resMsg = util.format ('%s RES %s\n\t%s'[color], new Date().toISOString(), res.statusCode, res.string[color] )
           console.log(reqMsg)
           console.log(resMsg)
         } else {
-          var reqMsg = util.format ('%s REQ %s\n\t%s', req.ts.toISOString(), req.method + ' ' + req.url, req.body )
-          var resMsg = util.format ('%s RES %s\n\t%s', new Date().toISOString(), res.statusCode, res.body)
+          var reqMsg = util.format ('%s REQ %s\n\t%s', new Date().toISOString(), req.method + ' ' + req.url, req.string )
+          var resMsg = util.format ('%s RES %s\n\t%s', new Date().toISOString(), res.statusCode, res.string)
           console.log(reqMsg)
           console.log(resMsg)
         }
-        
-    
-    }
-    
-    
-    proxy.web(req, res, {
-      target: target || 'http://127.0.0.1:9200'
-    })
-  });
-
-http.createServer(app).listen(port, function(){
-  console.log('proxy listen ' + port);
-});
-
-
-
+})
 
 
 
